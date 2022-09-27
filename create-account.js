0dcba3bc745e1bdc8d67c0df0b9894352ea3d0b6
Quentin Ikeno <ikenoq@gmail.com>
2022-09-26 17:49:24 -1000

    add withdraw method.

--- create-account.js
+++ create-account.js
@@ -10,9 +10,18 @@ function createAccount(pin, balance = 0) {
 		return `Succesfully deposited $${amount}. Current balance: $${balance}.`;
 	};
 
+	withdraw = (inputPin, amount) => {
+		if (inputPin !== pin) return "Invalid PIN.";
+		if (amount > balance)
+			return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+		balance -= amount;
+		return `Succesfully withdrew $${amount}. Current balance: $${balance}.`;
+	};
+
 	return {
 		checkBalance,
 		deposit,
+		withdraw,
 	};
 }
 
