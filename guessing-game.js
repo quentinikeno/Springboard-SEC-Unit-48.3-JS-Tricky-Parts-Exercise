8a2d06ac1ef6f874ec69b2c9a3e7943c08bf340a
Quentin Ikeno <ikenoq@gmail.com>
2022-09-26 17:06:18 -1000

    Write guessing game function.

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,22 @@
 function guessingGame() {
+	// make a random number
+	const target = Math.floor(Math.random() * 100);
+	let numGuesses = 0;
+	let hasWon = false;
 
+	// return a function
+	return function game(guess) {
+		if (hasWon) return "The game is over, you already won!";
+
+		numGuesses++;
+		if (guess === target) {
+			hasWon = true;
+			return `You win! You found ${target} in ${numGuesses} guesses.`;
+		}
+		return guess < target
+			? `${guess} is too low!`
+			: `${guess} is too high!`;
+	};
 }
 
 module.exports = { guessingGame };
