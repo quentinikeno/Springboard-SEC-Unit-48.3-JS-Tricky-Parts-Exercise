32a2f26eb6a8b22a6f56260804017847d9cf371f
Quentin Ikeno <ikenoq@gmail.com>
2022-09-26 17:46:56 -1000

    add checkBalance and deposit methods."

--- create-account.js
+++ create-account.js
@@ -1,5 +1,19 @@
-function createAccount(pin, amount) {
+function createAccount(pin, balance = 0) {
+	checkBalance = (inputPin) => {
+		if (inputPin !== pin) return "Invalid PIN.";
+		return `$${balance}`;
+	};
 
+	deposit = (inputPin, amount) => {
+		if (inputPin !== pin) return "Invalid PIN.";
+		balance += amount;
+		return `Succesfully deposited $${amount}. Current balance: $${balance}.`;
+	};
+
+	return {
+		checkBalance,
+		deposit,
+	};
 }
 
 module.exports = { createAccount };
