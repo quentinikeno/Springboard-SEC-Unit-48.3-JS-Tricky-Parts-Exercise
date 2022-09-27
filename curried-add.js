0ca82203ddbdb4891c17963592bcc2b2d4641bdd
Quentin Ikeno <ikenoq@gmail.com>
2022-09-26 17:58:53 -1000

    Write curriedAdd function.

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,10 @@
 function curriedAdd(total) {
-
+	if (total === undefined) return 0;
+	return function add(next) {
+		if (next === undefined) return total;
+		total += next;
+		return add;
+	};
 }
 
 module.exports = { curriedAdd };
